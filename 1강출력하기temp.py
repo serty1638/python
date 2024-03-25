60799a8921fc5fc59ded8be9668d6e0b8afe3c14
serty1638 <164852466+serty1638@users.noreply.github.com>
2024-03-25 17:24:24 +0900

    Add files via upload

---
+++ 1강출력하기temp.py
@@ -0,0 +1,65 @@
+from cospaces import *
+import math
+import random
+
+
+
+
+###print("num1xnum2 =",num1*num2)
+#print("num1/num2 =",num1/num2)
+##print("num1//num2 =",num1//num2)
+#print("num1%num2 =",num1%num2)
+#print("num1+num2 =",num1+num2)
+
+#hight = 180
+
+#print("hight =",hight)
+#print("hight+2 =",hight+2)
+#print("hight*2 =",hight*2)
+#print("hight/2 =",hight/2)
+#print("hight-2 =",hight-2)
+#print("hight%2 =",hight%2)
+#print("hight//2 =",hight//2)
+
+
+
+num1 = 7
+num2 = 3
+
+
+
+print("num1 =",num1)
+print("num2 =",num2)
+#print("num1 > num2 = ",num1 > num2)
+#print("num1 < num2 = ",num1 < num2)
+#print("num1 == num2 = ",num1 == num2)
+#print("num1 != num2 = ",num1 != num2)
+#print("num1 >= num2 = ",num1 >= num2)
+#print("num1 <= num2 = ",num1 <= num2)
+
+print("num1 + num2  =",num1+num2)
+print("num1 / num2  =",num1/num2)
+print("num1 * num2  =",num1*num2)
+print("num1 // num2  =",num1//num2)
+print("num1 % num2  =",num1%num2)
+print("==================================")
+
+#num1 = 4
+#print(num1 + num2)
+
+#num2 = 5
+#print(num2) 
+
+num1 = 12
+num2 = 8
+
+print("num1 =",num1)
+print("num2 =",num2)
+print("num1 + num2  =",num1+num2)
+print("num1 / num2  =",num1/num2)
+print("num1 * num2  =",num1*num2)
+print("num1 // num2  =",num1//num2)
+print("num1 % num2  =",num1%num2)
+
+
+
